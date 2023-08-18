67705da4533b6b23c60cd85a58ab44f8b8655865
Jaemin Choi <jaeminc@nvidia.com>
2023-08-18 15:18:42 -0700

    FusedAdam: Add FP32 master weights support for non-capturable case

--- apex/optimizers/fused_adam.py
+++ apex/optimizers/fused_adam.py
@@ -56,8 +56,7 @@ class FusedAdam(torch.optim.Optimizer):
         capturable (bool, optional): whether to use the version of the optimizer
             that can be used with CUDA Graphs. (default: False)
         master_weights (bool, optional): whether to maintain FP32 master weights
-           in the optimizer with FP16 mixed precision training, currently can
-           only be used with capturable set to True. (default: False)
+           in the optimizer with FP16 mixed precision training. (default: False)
 
     .. _Adam - A Method for Stochastic Optimization:
         https://arxiv.org/abs/1412.6980
@@ -72,8 +71,6 @@ class FusedAdam(torch.optim.Optimizer):
 
         if amsgrad:
             raise RuntimeError('FusedAdam does not support the AMSGrad variant.')
-        if master_weights and not capturable:
-            raise RuntimeError('Master weights is currently only supported with the capturable version.')
         # If the optimizer is capturable then LR should be a tensor (on GPU)
         lr = torch.tensor(lr, dtype=torch.float32) if capturable else lr
         defaults = dict(lr=lr, bias_correction=bias_correction,
@@ -91,7 +88,7 @@ class FusedAdam(torch.optim.Optimizer):
             param_list = pg['params']
             self.param_groups_master.append({
                 'params': [
-                    p.clone().detach().float() if self.master_weights else None
+                    p.clone().detach().float() if self.master_weights and (p.dtype == torch.float16) else None
                     for p in param_list
                 ],
             })
@@ -111,6 +108,7 @@ class FusedAdam(torch.optim.Optimizer):
             # Skip buffer
             self._dummy_overflow_buf = torch.cuda.IntTensor([0])
             self.multi_tensor_adam = amp_C.multi_tensor_adam
+            self.multi_tensor_adam_master = amp_C.multi_tensor_adam_master
             self.multi_tensor_adam_capturable = amp_C.multi_tensor_adam_capturable
             self.multi_tensor_adam_capturable_master = amp_C.multi_tensor_adam_capturable_master
         else:
@@ -158,7 +156,6 @@ class FusedAdam(torch.optim.Optimizer):
             g_bf, p_bf, m_bf, v_bf = [], [], [], []
             g_32, p_32, m_32, v_32 = [], [], [], []
             p_16_master = []
-            p_32_master = []
 
             for p, p_master in zip(group['params'], group_master['params']):
                 if p.grad is None:
@@ -187,8 +184,6 @@ class FusedAdam(torch.optim.Optimizer):
                     m_bf.append(state['exp_avg'])
                     v_bf.append(state['exp_avg_sq'])
                 elif p.dtype == torch.float32:
-                    if self.master_weights:
-                        p_32_master.append(p_master.data)
                     g_32.append(p.grad.data)
                     p_32.append(p.data)
                     m_32.append(state['exp_avg'])
@@ -247,11 +242,9 @@ class FusedAdam(torch.optim.Optimizer):
                             inv_scale)
 
                 if len(g_32) > 0:
-                    multi_tensor_applier(self.multi_tensor_adam_capturable_master if self.master_weights
-                            else self.multi_tensor_adam_capturable,
+                    multi_tensor_applier(self.multi_tensor_adam_capturable,
                             self._dummy_overflow_buf,
-                            [g_32, p_32, m_32, v_32, p_32_master] if self.master_weights
-                            else [g_32, p_32, m_32, v_32],
+                            [g_32, p_32, m_32, v_32],
                             group['lr'],
                             beta1,
                             beta2,
@@ -263,9 +256,11 @@ class FusedAdam(torch.optim.Optimizer):
                             inv_scale)
             else:
                 if len(g_16) > 0:
-                    multi_tensor_applier(self.multi_tensor_adam,
+                    multi_tensor_applier(self.multi_tensor_adam_master if self.master_weights
+                            else self.mult_tensor_adam,
                             self._dummy_overflow_buf,
-                            [g_16, p_16, m_16, v_16],
+                            [g_16, p_16, m_16, v_16, p16_master] if self.master_weights
+                            else [g_16, p_16, m_16, v_16],
                             group['lr'],
                             beta1,
                             beta2,
